cae950f6c75827911cb219f9c1c3b6a3242b76be
yeshwanthsatyadev <yeshwanth.dev9@gmail.com>
2021-11-21 11:39:20 +0530

    split_join file commit

---
+++ pythonProject1/split_join.py
@@ -0,0 +1,11 @@
+def split_and_join(line):
+    # write your code here
+    Output = line.split();
+    Output = "-".join(Output)
+    return Output;
+
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
